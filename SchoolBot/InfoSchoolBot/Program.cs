6abf364dc48834c06d16d3fd6690506087e62a25
Fedor <pichuginf2016@mail.ru>
2022-12-05 19:57:47 +0500

    trying logging

--- SchoolBot/InfoSchoolBot/Program.cs
+++ SchoolBot/InfoSchoolBot/Program.cs
@@ -1,4 +1,5 @@
-﻿using Serilog;
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
 
 namespace SchoolBot;
 
@@ -7,11 +8,24 @@ static class Program
     private static void Main()
     {
         Console.WriteLine();
+        var builder = new ConfigurationBuilder();
+        BuildConfig(builder);
+        
         using var logger = new LoggerConfiguration()
-            .WriteTo.SQLite(Environment.CurrentDirectory.Replace(@"SchoolBot\InfoSchoolBot\bin\Debug\net6.0", ""))
-            .WriteTo.Console()
+            .ReadFrom.Configuration(builder.Build())
+            //.Enrich.FromLogContext()
+            .WriteTo.SQLite(Environment.CurrentDirectory.Replace(@"SchoolBot\InfoSchoolBot\bin\Debug\net6.0", @"LogDataBase\Logs.db"))
+            // .WriteTo.Console()
             .CreateLogger();
+        
         logger.Information("Started StolovkaBot");
+        
         BotClient.StartBot();
+    }
+    private static void BuildConfig(IConfigurationBuilder builder)
+    {
+        builder.SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
     }
 }
